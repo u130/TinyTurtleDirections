05f2041416724adbd8d6777777a8ad13caba6765
Usman Adeel <adeel@citypolyhs.org>
2017-01-18 21:28:34 +0000

    house

---
+++ script.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window); 
+
+forward(20);
+
+right(90);
+
+forward(20);
+left(50)
+forward(60)
+right(90)
+forward(80)
+left(40)
+forward(20)
+right(90)
+forward(20)
+right(90)
+forward(140)
